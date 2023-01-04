5a0b3a9a47a6d7b50687777d14c4a5edc1fd3175
David <David@DAVID-PC>
2023-01-04 16:09:17 +0100

    Add project files.

---
+++ Yahtzee/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Yahtzee
+{
+    partial class Yahtzee
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_Roll = new System.Windows.Forms.Button();
+            this.PCB_Dice1 = new System.Windows.Forms.PictureBox();
+            this.PCB_Dice2 = new System.Windows.Forms.PictureBox();
+            this.PCB_Dice3 = new System.Windows.Forms.PictureBox();
+            this.PCB_Dice4 = new System.Windows.Forms.PictureBox();
+            this.PCB_Dice5 = new System.Windows.Forms.PictureBox();
+            this.CKB_DiceKeeper1 = new System.Windows.Forms.CheckBox();
+            this.CKB_DiceKeeper2 = new System.Windows.Forms.CheckBox();
+            this.CKB_DiceKeeper3 = new System.Windows.Forms.CheckBox();
+            this.CKB_DiceKeeper4 = new System.Windows.Forms.CheckBox();
+            this.CKB_DiceKeeper5 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.test = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTN_Roll
+            // 
+            this.BTN_Roll.Location = new System.Drawing.Point(244, 352);
+            this.BTN_Roll.Name = "BTN_Roll";
+            this.BTN_Roll.Size = new System.Drawing.Size(100, 52);
+            this.BTN_Roll.TabIndex = 1;
+            this.BTN_Roll.Text = "Roll";
+            this.BTN_Roll.UseVisualStyleBackColor = true;
+            this.BTN_Roll.Click += new System.EventHandler(this.BTN_Roll_Click);
+            // 
+            // PCB_Dice1
+            // 
+            this.PCB_Dice1.Location = new System.Drawing.Point(32, 115);
+            this.PCB_Dice1.Name = "PCB_Dice1";
+            this.PCB_Dice1.Size = new System.Drawing.Size(100, 100);
+            this.PCB_Dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PCB_Dice1.TabIndex = 2;
+            this.PCB_Dice1.TabStop = false;
+            // 
+            // PCB_Dice2
+            // 
+            this.PCB_Dice2.Location = new System.Drawing.Point(138, 115);
+            this.PCB_Dice2.Name = "PCB_Dice2";
+            this.PCB_Dice2.Size = new System.Drawing.Size(100, 100);
+            this.PCB_Dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PCB_Dice2.TabIndex = 3;
+            this.PCB_Dice2.TabStop = false;
+            // 
+            // PCB_Dice3
+            // 
+            this.PCB_Dice3.Location = new System.Drawing.Point(244, 115);
+            this.PCB_Dice3.Name = "PCB_Dice3";
+            this.PCB_Dice3.Size = new System.Drawing.Size(100, 100);
+            this.PCB_Dice3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PCB_Dice3.TabIndex = 4;
+            this.PCB_Dice3.TabStop = false;
+            // 
+            // PCB_Dice4
+            // 
+            this.PCB_Dice4.Location = new System.Drawing.Point(350, 115);
+            this.PCB_Dice4.Name = "PCB_Dice4";
+            this.PCB_Dice4.Size = new System.Drawing.Size(100, 100);
+            this.PCB_Dice4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PCB_Dice4.TabIndex = 5;
+            this.PCB_Dice4.TabStop = false;
+            // 
+            // PCB_Dice5
+            // 
+            this.PCB_Dice5.Location = new System.Drawing.Point(456, 115);
+            this.PCB_Dice5.Name = "PCB_Dice5";
+            this.PCB_Dice5.Size = new System.Drawing.Size(100, 100);
+            this.PCB_Dice5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PCB_Dice5.TabIndex = 6;
+            this.PCB_Dice5.TabStop = false;
+            // 
+            // CKB_DiceKeeper1
+            // 
+            this.CKB_DiceKeeper1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.CKB_DiceKeeper1.AutoSize = true;
+            this.CKB_DiceKeeper1.Location = new System.Drawing.Point(54, 221);
+            this.CKB_DiceKeeper1.Name = "CKB_DiceKeeper1";
+            this.CKB_DiceKeeper1.Size = new System.Drawing.Size(43, 25);
+            this.CKB_DiceKeeper1.TabIndex = 12;
+            this.CKB_DiceKeeper1.Text = "Keep";
+            this.CKB_DiceKeeper1.UseVisualStyleBackColor = true;
+            this.CKB_DiceKeeper1.CheckedChanged += new System.EventHandler(this.CKB_DiceKeeper1_CheckedChanged);
+            // 
+            // CKB_DiceKeeper2
+            // 
+            this.CKB_DiceKeeper2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.CKB_DiceKeeper2.AutoSize = true;
+            this.CKB_DiceKeeper2.Location = new System.Drawing.Point(164, 221);
+            this.CKB_DiceKeeper2.Name = "CKB_DiceKeeper2";
+            this.CKB_DiceKeeper2.Size = new System.Drawing.Size(43, 25);
+            this.CKB_DiceKeeper2.TabIndex = 13;
+            this.CKB_DiceKeeper2.Text = "Keep";
+            this.CKB_DiceKeeper2.UseVisualStyleBackColor = true;
+            this.CKB_DiceKeeper2.CheckedChanged += new System.EventHandler(this.CKB_DiceKeeper2_CheckedChanged);
+            // 
+            // CKB_DiceKeeper3
+            // 
+            this.CKB_DiceKeeper3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.CKB_DiceKeeper3.AutoSize = true;
+            this.CKB_DiceKeeper3.Location = new System.Drawing.Point(270, 221);
+            this.CKB_DiceKeeper3.Name = "CKB_DiceKeeper3";
+            this.CKB_DiceKeeper3.Size = new System.Drawing.Size(43, 25);
+            this.CKB_DiceKeeper3.TabIndex = 14;
+            this.CKB_DiceKeeper3.Text = "Keep";
+            this.CKB_DiceKeeper3.UseVisualStyleBackColor = true;
+            this.CKB_DiceKeeper3.CheckedChanged += new System.EventHandler(this.CKB_DiceKeeper3_CheckedChanged);
+            // 
+            // CKB_DiceKeeper4
+            // 
+            this.CKB_DiceKeeper4.Appearance = System.Windows.Forms.Appearance.Button;
+            this.CKB_DiceKeeper4.AutoSize = true;
+            this.CKB_DiceKeeper4.Location = new System.Drawing.Point(374, 221);
+            this.CKB_DiceKeeper4.Name = "CKB_DiceKeeper4";
+            this.CKB_DiceKeeper4.Size = new System.Drawing.Size(43, 25);
+            this.CKB_DiceKeeper4.TabIndex = 15;
+            this.CKB_DiceKeeper4.Text = "Keep";
+            this.CKB_DiceKeeper4.UseVisualStyleBackColor = true;
+            this.CKB_DiceKeeper4.CheckedChanged += new System.EventHandler(this.CKB_DiceKeeper4_CheckedChanged);
+            // 
+            // CKB_DiceKeeper5
+            // 
+            this.CKB_DiceKeeper5.Appearance = System.Windows.Forms.Appearance.Button;
+            this.CKB_DiceKeeper5.AutoSize = true;
+            this.CKB_DiceKeeper5.Location = new System.Drawing.Point(485, 221);
+            this.CKB_DiceKeeper5.Name = "CKB_DiceKeeper5";
+            this.CKB_DiceKeeper5.Size = new System.Drawing.Size(43, 25);
+            this.CKB_DiceKeeper5.TabIndex = 16;
+            this.CKB_DiceKeeper5.Text = "Keep";
+            this.CKB_DiceKeeper5.UseVisualStyleBackColor = true;
+            this.CKB_DiceKeeper5.CheckedChanged += new System.EventHandler(this.CKB_DiceKeeper5_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.test,
+            this.P1,
+            this.P2});
+            this.dataGridView1.Location = new System.Drawing.Point(629, 96);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(244, 150);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // test
+            // 
+            this.test.HeaderText = "";
+            this.test.Name = "test";
+            // 
+            // P1
+            // 
+            this.P1.HeaderText = "Player1";
+            this.P1.Name = "P1";
+            this.P1.Width = 50;
+            // 
+            // P2
+            // 
+            this.P2.HeaderText = "Player2";
+            this.P2.Name = "P2";
+            this.P2.Width = 50;
+            // 
+            // Yahtzee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(904, 530);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.CKB_DiceKeeper5);
+            this.Controls.Add(this.CKB_DiceKeeper4);
+            this.Controls.Add(this.CKB_DiceKeeper3);
+            this.Controls.Add(this.CKB_DiceKeeper2);
+            this.Controls.Add(this.CKB_DiceKeeper1);
+            this.Controls.Add(this.PCB_Dice5);
+            this.Controls.Add(this.PCB_Dice4);
+            this.Controls.Add(this.PCB_Dice3);
+            this.Controls.Add(this.PCB_Dice2);
+            this.Controls.Add(this.PCB_Dice1);
+            this.Controls.Add(this.BTN_Roll);
+            this.Name = "Yahtzee";
+            this.Text = "Yahtzee";
+            this.Load += new System.EventHandler(this.Yahtzee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PCB_Dice5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button BTN_Roll;
+        private PictureBox PCB_Dice1;
+        private PictureBox PCB_Dice2;
+        private PictureBox PCB_Dice3;
+        private PictureBox PCB_Dice4;
+        private PictureBox PCB_Dice5;
+        private CheckBox CKB_DiceKeeper1;
+        private CheckBox CKB_DiceKeeper2;
+        private CheckBox CKB_DiceKeeper3;
+        private CheckBox CKB_DiceKeeper4;
+        private CheckBox CKB_DiceKeeper5;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn test;
+        private DataGridViewTextBoxColumn P1;
+        private DataGridViewTextBoxColumn P2;
+    }
+}
---
+++ Yahtzee/Form1.cs
@@ -0,0 +1,287 @@
+namespace Yahtzee
+{
+    public partial class Yahtzee : Form
+    {
+        public Yahtzee()
+        {
+            InitializeComponent();
+            CKB_DiceKeeper1.Visible = false;
+            CKB_DiceKeeper2.Visible = false;
+            CKB_DiceKeeper3.Visible = false;
+            CKB_DiceKeeper4.Visible = false;
+            CKB_DiceKeeper5.Visible = false;
+        }
+
+        private static Random RNG = new Random();
+        private static int[] StoreTMB = new int[5];
+
+        private static bool DiceKeep1 = false;
+        private static bool DiceKeep2 = false;
+        private static bool DiceKeep3 = false;
+        private static bool DiceKeep4 = false;
+        private static bool DiceKeep5 = false;
+
+        private void Yahtzee_Load(object sender, EventArgs e)
+        {
+            //PCB_DiceX
+            //BTN_DiceKeeperX
+        }
+
+        // Dice Roller
+        private static int RollsRemaining = 2;
+
+        private static void kategoria()
+        {
+            //egyesek
+            int sumegyesek = 0;
+            foreach (int i in StoreTMB)
+            {
+                if (StoreTMB[i] == 1) sumegyesek += i;
+            }
+            //kettesek
+            int sumkettesek = 0;
+            foreach (int i in StoreTMB)
+            {
+                if (StoreTMB[i] == 2) sumkettesek += i;
+            }
+        }
+
+        private void BTN_Roll_Click(object sender, EventArgs e)
+        {
+            // Checks if out of rolls
+            if (RollsRemaining == 0)
+            {
+                BTN_Roll.Enabled = false;
+                BTN_Roll.Text = "Out Of Rolls";
+            }
+
+            for (int i = 0; i < StoreTMB.Length; i++)
+            {
+                StoreTMB[i] = RNG.Next(1, 7);
+            }
+            CKB_DiceKeeper1.Visible = true;
+            CKB_DiceKeeper2.Visible = true;
+            CKB_DiceKeeper3.Visible = true;
+            CKB_DiceKeeper4.Visible = true;
+            CKB_DiceKeeper5.Visible = true;
+
+            #region DICE
+
+            //Dice 1 Faces
+
+            if (DiceKeep1 == false)
+            {
+                if (StoreTMB[0] == 1)
+                {
+                    PCB_Dice1.Image = Image.FromFile($@"..\..\..\RES\Dice1.png");
+                    PCB_Dice1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[0] == 2)
+                {
+                    PCB_Dice1.Image = Image.FromFile(@"..\..\..\RES\Dice2.png");
+                    PCB_Dice1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[0] == 3)
+                {
+                    PCB_Dice1.Image = Image.FromFile(@"..\..\..\RES\Dice3.png");
+                    PCB_Dice1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[0] == 4)
+                {
+                    PCB_Dice1.Image = Image.FromFile(@"..\..\..\RES\Dice4.png");
+                    PCB_Dice1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[0] == 5)
+                {
+                    PCB_Dice1.Image = Image.FromFile(@"..\..\..\RES\Dice5.png");
+                    PCB_Dice1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[0] == 6)
+                {
+                    PCB_Dice1.Image = Image.FromFile(@"..\..\..\RES\Dice6.png");
+                    PCB_Dice1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+
+            //Dice 2 Faces
+            if (DiceKeep2 == false)
+            {
+                if (StoreTMB[1] == 1)
+                {
+                    PCB_Dice2.Image = Image.FromFile($@"..\..\..\RES\Dice1.png");
+                    PCB_Dice2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[1] == 2)
+                {
+                    PCB_Dice2.Image = Image.FromFile(@"..\..\..\RES\Dice2.png");
+                    PCB_Dice2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[1] == 3)
+                {
+                    PCB_Dice2.Image = Image.FromFile(@"..\..\..\RES\Dice3.png");
+                    PCB_Dice2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[1] == 4)
+                {
+                    PCB_Dice2.Image = Image.FromFile(@"..\..\..\RES\Dice4.png");
+                    PCB_Dice2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[1] == 5)
+                {
+                    PCB_Dice2.Image = Image.FromFile(@"..\..\..\RES\Dice5.png");
+                    PCB_Dice2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[1] == 6)
+                {
+                    PCB_Dice2.Image = Image.FromFile(@"..\..\..\RES\Dice6.png");
+                    PCB_Dice2.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+
+            //Dice 3 Faces
+            if (DiceKeep3 == false)
+            {
+                if (StoreTMB[2] == 1)
+                {
+                    PCB_Dice3.Image = Image.FromFile($@"..\..\..\RES\Dice1.png");
+                    PCB_Dice3.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[2] == 2)
+                {
+                    PCB_Dice3.Image = Image.FromFile(@"..\..\..\RES\Dice2.png");
+                    PCB_Dice3.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[2] == 3)
+                {
+                    PCB_Dice3.Image = Image.FromFile(@"..\..\..\RES\Dice3.png");
+                    PCB_Dice3.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[2] == 4)
+                {
+                    PCB_Dice3.Image = Image.FromFile(@"..\..\..\RES\Dice4.png");
+                    PCB_Dice3.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[2] == 5)
+                {
+                    PCB_Dice3.Image = Image.FromFile(@"..\..\..\RES\Dice5.png");
+                    PCB_Dice3.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[2] == 6)
+                {
+                    PCB_Dice3.Image = Image.FromFile(@"..\..\..\RES\Dice6.png");
+                    PCB_Dice3.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+
+            //Dice 4 Faces
+            if (DiceKeep4 == false)
+            {
+                if (StoreTMB[3] == 1)
+                {
+                    PCB_Dice4.Image = Image.FromFile($@"..\..\..\RES\Dice1.png");
+                    PCB_Dice4.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[3] == 2)
+                {
+                    PCB_Dice4.Image = Image.FromFile(@"..\..\..\RES\Dice2.png");
+                    PCB_Dice4.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[3] == 3)
+                {
+                    PCB_Dice4.Image = Image.FromFile(@"..\..\..\RES\Dice3.png");
+                    PCB_Dice4.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[3] == 4)
+                {
+                    PCB_Dice4.Image = Image.FromFile(@"..\..\..\RES\Dice4.png");
+                    PCB_Dice4.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[3] == 5)
+                {
+                    PCB_Dice4.Image = Image.FromFile(@"..\..\..\RES\Dice5.png");
+                    PCB_Dice4.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[3] == 6)
+                {
+                    PCB_Dice4.Image = Image.FromFile(@"..\..\..\RES\Dice6.png");
+                    PCB_Dice4.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+
+            //Dice 5 Faces
+            if (DiceKeep5 == false)
+            {
+                if (StoreTMB[4] == 1)
+                {
+                    PCB_Dice5.Image = Image.FromFile($@"..\..\..\RES\Dice1.png");
+                    PCB_Dice5.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[4] == 2)
+                {
+                    PCB_Dice5.Image = Image.FromFile(@"..\..\..\RES\Dice2.png");
+                    PCB_Dice5.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[4] == 3)
+                {
+                    PCB_Dice5.Image = Image.FromFile(@"..\..\..\RES\Dice3.png");
+                    PCB_Dice5.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[4] == 4)
+                {
+                    PCB_Dice5.Image = Image.FromFile(@"..\..\..\RES\Dice4.png");
+                    PCB_Dice5.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[4] == 5)
+                {
+                    PCB_Dice5.Image = Image.FromFile(@"..\..\..\RES\Dice5.png");
+                    PCB_Dice5.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                if (StoreTMB[4] == 6)
+                {
+                    PCB_Dice5.Image = Image.FromFile(@"..\..\..\RES\Dice6.png");
+                    PCB_Dice5.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+            }
+
+            #endregion DICE
+
+            RollsRemaining--;
+        }
+
+        //Dice 1 Keeper
+        private void CKB_DiceKeeper1_CheckedChanged(object sender, EventArgs e)
+        {
+            DiceKeep1 = true;
+            if (CKB_DiceKeeper1.Checked == false)
+                DiceKeep1 = false;
+        }
+
+        private void CKB_DiceKeeper2_CheckedChanged(object sender, EventArgs e)
+        {
+            DiceKeep2 = true;
+            if (CKB_DiceKeeper2.Checked == false)
+                DiceKeep2 = false;
+        }
+
+        private void CKB_DiceKeeper3_CheckedChanged(object sender, EventArgs e)
+        {
+            DiceKeep3 = true;
+            if (CKB_DiceKeeper3.Checked == false)
+                DiceKeep3 = false;
+        }
+
+        private void CKB_DiceKeeper4_CheckedChanged(object sender, EventArgs e)
+        {
+            DiceKeep4 = true;
+            if (CKB_DiceKeeper4.Checked == false)
+                DiceKeep4 = false;
+        }
+
+        private void CKB_DiceKeeper5_CheckedChanged(object sender, EventArgs e)
+        {
+            DiceKeep5 = true;
+            if (CKB_DiceKeeper5.Checked == false)
+                DiceKeep5 = false;
+        }
+    }
+}
